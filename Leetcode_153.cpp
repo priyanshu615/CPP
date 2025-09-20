477838bb8390e60d7ef7b2fcadc324f1f190260e
Priyanshu Mehta <priyanshumehta294@gmail.com>
2025-09-21 03:17:59 +0530

    New Problem added

---
+++ Leetcode_153.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+ int findMin(vector<int>& nums) {
+
+       int start = 0, end = nums.size()-1,mid,ans = nums[0];
+
+        while(start<=end){
+
+            mid = start + (end-start)/2;
+
+            if(nums[mid]>=nums[0]){
+                start = mid+1;
+            }
+
+            else {
+              ans = nums[mid];
+              end = mid - 1;
+            }
+        }
+
+        return ans;
+        
+    }
+
+int main() {
+    int n;
+    cout<<"Enter size of array : ";
+    cin>>n;
+
+    vector<int> arr(n);
+
+    cout<<"Enter elements in array : ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int result = findMin(arr);
+    cout<<"Minimum element in rotated array is : "<<result<<endl;
+
+    
+}
